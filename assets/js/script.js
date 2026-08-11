70f6ebdb9404f35009ace5a4bc5adba5d133e519
Mika <mikawittmann@gmx.de>
2026-08-11 18:39:27 +0200

    Login mit Passwort, Buttonklick macht Eintrag

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,10 +17,6 @@ setInterval(uhrzeitAnzeigen, 1000);
 
 
 
-
-
-
-
 const supabaseUrl = "https://mcdelwjwyrahrufjgnvu.supabase.co";
 const supabaseKey = "sb_publishable_HBhsRhWExorIvdq4DHbO1Q_T1FOXofw";
 
@@ -28,6 +24,11 @@ const supabaseClient = window.supabase.createClient(
     supabaseUrl,
     supabaseKey
 );
+
+
+
+
+
 
 async function getPlayers() {
 
@@ -37,11 +38,9 @@ async function getPlayers() {
         .order("nummer", { ascending: true });
 
     if (error) {
-        console.error("Fehler:", error);
+        console.error("Fehler beim Laden der Spieler:", error);
         return;
     }
-
-    console.log("Spieler:", data);
 
     const playersContainer = document.getElementById("players");
 
@@ -53,8 +52,84 @@ async function getPlayers() {
 
         button.textContent = player.nummer;
 
+        // Spieler beim Klick speichern
+        button.addEventListener("click", () => {
+            createInvoice(player);
+        });
+
         playersContainer.appendChild(button);
     });
 }
 
+
+
+
+
+
+
+
+
+async function createInvoice(player) {
+
+    const { data, error } = await supabaseClient
+        .from("invoices")
+        .insert({
+            id: player.id,
+            name: player.name,
+            vorname: player.vorname,
+            number: player.nummer,
+            paid: false,
+            time: new Date().toISOString(),
+            gameday: false
+        })
+        .select();
+
+    if (error) {
+        console.error("Fehler beim Erstellen der Invoice:", error);
+        return;
+    }
+
+    console.log("Invoice erstellt:", data);
+}
+
 getPlayers();
+
+
+
+
+
+
+
+
+
+async function login() {
+    const password = document.getElementById("zugang").value;
+
+    const { data, error } = await supabaseClient
+        .from("passwords")
+        .select("id")
+        .eq("password", password)
+        .maybeSingle();
+
+    if (error) {
+        console.error(error);
+        alert("Fehler beim Login.");
+        return;
+    }
+
+    if (data) {
+        window.location.href = "players.html";
+    } else {
+        alert("Falsches Passwort.");
+    }
+}
+
+
+
+
+
+
+function menuFunction(x) {
+    x.classList.toggle("change");
+    // document.getElementById("navLinks").classList.toggle("show");
+}
